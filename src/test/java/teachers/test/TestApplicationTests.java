ece7e80794b615ce6a4b8d95987e46ef8e555e86
lian08 <lian080408@gmail.com>
2025-07-23 21:11:50 +0900

    Initial Commit

---
+++ src/test/java/teachers/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package teachers.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
